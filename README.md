50759f855782d90838d58a5075a33111f1e0dd24
Jordyn <jlewis162030@gmail.com>
2023-06-28 11:15:14 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# empirestatebuildingcolors
+Fetches the current color scheme for the lights on the [Empire State Building](https://www.esbnyc.com). From the color scheme description, we use OpenAI's API to extract some list of hex codes that best match the description.
+
+## How to Use
+Make sure [npm](https://docs.npmjs.com/downloading-and-installing-node-js-and-npm) is installed on your machine.
+
+Navigate to the project directory and run `npm install` to install all dependecies.
+
+You will need to retrieve your own [OpenAI API key](https://openai.com/blog/openai-api).
+
+Paste your API key into the empty string found in `config.json` in the root directory of the project.
+
+Run `node index.js` to execute.
+
+## OpenAI API Caveats
+The OpenAI API is not free, however it is not necessarily expensive either. Pricing varies based on the model that you choose to run, however it is recommended to not change the current model used (gpt-3.5-turbo), as it on the cheaper end with high accuracy.
---
+++ index.js
@@ -0,0 +1,72 @@
+const cheerio = require('cheerio');
+const { Configuration, OpenAIApi } = require('openai');
+const config = require('./config.json');
+const configuration = new Configuration({
+    apiKey: config.apiKey,
+});
+const openai = new OpenAIApi(configuration);
+
+main();
+
+// Methods ////////////////////////////////////////////
+
+async function main() {
+    const colorDescription = await grabESBColor();
+    const hexCodeMessage = await getHexCode(colorDescription);
+    const hexCodeArray = parseHexCodes(hexCodeMessage);
+    console.log(hexCodeArray);
+}
+
+/**
+ * Grabs the color of the Empire State Building
+ * @returns {string} The color of the Empire State Building
+ */
+async function grabESBColor() {
+    let ESBResponse = await fetch('https://www.esbnyc.com');
+    let ESBResponseText = await ESBResponse.text();
+    const $ = cheerio.load(ESBResponseText);
+    // Fetch the element by class name
+    const colorDescription = $('.light-type').text();
+    return "Peanut butter brown";
+}
+
+/**
+ * Send message to OpenAI API asking for a hex code for the color described
+ * and return the hex code.
+ * @param {string} colorDescription The color description of the Empire State Building
+ * @returns {string} The hex code for the color described
+ */
+async function getHexCode(colorDescription) {
+    const promptMsg = `Provide simple hex code(s) for the color scheme: \"${colorDescription}\".`;
+    console.log(promptMsg);
+    const response = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{"role": "system", "content": promptMsg}],
+        max_tokens: 64,
+        temperature: 0,
+    });
+    console.log(response.data.choices);
+    return response.data.choices[0].message.content;
+}
+
+/**
+ * Sometimes, the model likes to add some additional text to its response. 
+ * Parse the hex codes from the message returned from the OpenAI API,
+ * and add each hexcode string to an array. Return the array.
+ * @param {string} hexCodeMessage The message returned from the OpenAI API
+ * @returns {string[]} An array of hex codes
+ */
+function parseHexCodes(hexCodeMessage) {
+    let hexArray = [];
+    for (let i = 0; i < hexCodeMessage.length; i++) {
+        if (hexCodeMessage[i] == "#") {
+            let hexCode = hexCodeMessage.slice(i, i + 7);
+            i += 7;
+            hexArray.push(hexCode);
+        }
+    }
+    return hexArray;
+}
+
+// how do I git remote add origin
+// how do I git push -u origin main
