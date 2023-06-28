8691d1554534a78dff4277ec29070d85ec13a354
Jordyn <jlewis162030@gmail.com>
2023-06-28 18:06:36 -0500

    comments

--- index.js
+++ index.js
@@ -15,12 +15,6 @@ async function main() {
      */
     const colorDescription = await grabESBColor();
 
-    // Extract a hex code color scheme from the color description.
-    // If the color description exists in the colorMap.json file, we 
-    // will return the hex code from the colorMap.json file. Otherwise,
-    // we will send a message to the OpenAI API to derive a hex code color scheme
-    // and then save the new color scheme result to the color map to save tokens.
-
     /**
      * Extract a hex code color scheme from the color description.
      * If the color description exists in the colorMap.json file, we 
